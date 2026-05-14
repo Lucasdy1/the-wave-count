fd4417f50cee8a9c1026ffa5ece61e61f1b740d8
Lucasdy1 <lucasdy32@gmail.com>
2026-05-15 00:23:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -613,16 +613,14 @@ realizedReturn: oldPosition?.realizedReturn ?? 0,
     </tr>
   );
 })}
-    {tab === 'closed'
-      ? `${entry.weight - entry.remainingWeight} / ${entry.weight}`
-      : `${entry.remainingWeight} / ${entry.weight}`}
-  </td>
-  <td>{entry.direction}</td>
   <td>{entry.entryDate}</td>
   <td>{money.format(entry.avgEntry)}</td>
   <td>{entry.current ? money.format(entry.current) : 'Waiting for API'}</td>
   <td className={entryPerf >= 0 ? 'green' : 'red'}>{pct(entryPerf)}</td>
 </tr>
+);
+})}
+</React.Fragment>
 
   {entry.remainingWeight} / {entry.weight}
 
