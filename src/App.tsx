27ad1f0a2231ddeb5d9db31265c188373d65da92
Lucasdy1 <lucasdy32@gmail.com>
2026-05-15 11:06:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -584,7 +584,9 @@ realizedReturn: oldPosition?.realizedReturn ?? 0,
                       <td><b>{isExpanded ? '▾' : '▸'} {group.display}</b></td>
                       <td>{group.entries.length}</td>
                       <td>
-  {group.entries.reduce((sum, entry) => sum + entry.remainingWeight, 0)} / {group.entries.reduce((sum, entry) => sum + entry.weight, 0)}
+  {tab === 'closed'
+    ? `${group.entries.reduce((sum, entry) => sum + (entry.weight - entry.remainingWeight), 0)} / ${group.entries.reduce((sum, entry) => sum + entry.weight, 0)}`
+    : `${group.entries.reduce((sum, entry) => sum + entry.remainingWeight, 0)} / ${group.entries.reduce((sum, entry) => sum + entry.weight, 0)}`}
 </td>
                       <td>{group.direction}</td>
                       <td>{group.firstEntryDate}</td>
